ef3b12d24f41ff76098b6823742788a1ed14f173
Aniket Bhattacharyea <aniket@abhattacharyea.dev>
2026-06-06 21:10:44 +0530

    Add fingerprint

--- modules/hosts/horologium/system.nix
+++ modules/hosts/horologium/system.nix
@@ -68,6 +68,13 @@
 
       security.polkit.enable = true;
       services.avahi.hostName = "horologium";
+
+      # Install the driver
+      services.fprintd.enable = true;
+      # If simply enabling fprintd is not enough, try enabling fprintd.tod...
+      services.fprintd.tod.enable = true;
+      # ...and use one of the next four drivers
+      services.fprintd.tod.driver = pkgs.libfprint-2-tod1-goodix; # Goodix driver module
       system.stateVersion = "25.11";
     };
 

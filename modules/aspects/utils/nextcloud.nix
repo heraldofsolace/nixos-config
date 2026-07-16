fa4a701b32f6bc4aa7512e8077a1cdd3ee768dac
Aniket Bhattacharyea <aniket@abhattacharyea.dev>
2026-07-16 21:57:19 +0530

    Add Miranda

--- modules/aspects/utils/nextcloud.nix
+++ modules/aspects/utils/nextcloud.nix
@@ -1,45 +1,56 @@
 _: {
   blazar.utils._.nextcloud = {
-    nixos = { pkgs, config, ... }: {
-      services.nextcloud = {
-        enable = true;
-        package = pkgs.nextcloud33;
-        hostName = "miranda.dorper-ghost.ts.net";
-        https = true;
-        enableImagemagick = true;
-        database.createLocally = true;
-        appstoreEnable = true;
-        autoUpdateApps.enable = true;
-        config = {
-          dbtype = "pgsql";
-          adminpassFile = "/run/secrets/nextcloud-password";
-          defaultPhoneRegion = "IN";
+    nixos =
+      {
+        pkgs,
+        config,
+        ...
+      }:
+      {
+        nextcloud-password = {
+          key = "nextcloud";
+          sopsFile = ../../../secrets/user-passwords.yaml;
+          owner = "nextcloud";
         };
-        extraApps = with config.services.nextcloud.package.packages.apps; {
-          inherit
-            news
-            contacts
-            calendar
-            tasks
-            bookmarks
-            notes
-            deck
-            mail
-            twofactor_webauthn
-            ;
+        services.nextcloud = {
+          enable = true;
+          package = pkgs.nextcloud33;
+          hostName = "miranda.dorper-ghost.ts.net";
+          https = true;
+          enableImagemagick = true;
+          database.createLocally = true;
+          appstoreEnable = true;
+          autoUpdateApps.enable = true;
+          config = {
+            dbtype = "pgsql";
+            adminpassFile = "/run/secrets/nextcloud-password";
+            defaultPhoneRegion = "IN";
+          };
+          extraApps = with config.services.nextcloud.package.packages.apps; {
+            inherit
+              news
+              contacts
+              calendar
+              tasks
+              bookmarks
+              notes
+              deck
+              mail
+              twofactor_webauthn
+              ;
+          };
+          extraAppsEnable = true;
+          globalProfiles = true;
+          maxUploadSize = "5G";
+          notify_push.enable = true;
+          configureRedis = true;
         };
-        extraAppsEnable = true;
-        globalProfiles = true;
-        maxUploadSize = "5G";
-        notify_push.enable = true;
-        configureRedis = true;
-      };
 
-      services.nginx.virtualHosts.${config.services.nextcloud.hostName} = {
-        forceSSL = true;
-        sslCertificate = "/run/secrets/miranda-cert";
-        sslCertificateKey = "/run/secrets/miranda-cert-key";
+        services.nginx.virtualHosts.${config.services.nextcloud.hostName} = {
+          forceSSL = true;
+          sslCertificate = "/run/secrets/miranda-cert";
+          sslCertificateKey = "/run/secrets/miranda-cert-key";
+        };
       };
-    };
   };
 }

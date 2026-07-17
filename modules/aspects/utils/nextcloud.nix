f86ab9a187444f351164f0d448dd4f97fd330d42
Aniket Bhattacharyea <aniket@abhattacharyea.dev>
2026-07-17 13:36:12 +0530

    Add Miranda

--- modules/aspects/utils/nextcloud.nix
+++ modules/aspects/utils/nextcloud.nix
@@ -65,12 +65,12 @@ _: {
           configureRedis = true;
         };
 
-        services.onlyoffice = {
-          enable = true;
-          hostname = "onlyoffice.miranda.dorper-ghost.ts.net";
-          securityNonceFile = "/run/secrets/onlyoffice-nonce-file";
-          jwtSecretFile = "/run/secrets/onlyoffice-jwt-secret";
-        };
+        # services.onlyoffice = {
+        #   enable = true;
+        #   hostname = "onlyoffice.miranda.dorper-ghost.ts.net";
+        #   securityNonceFile = "/run/secrets/onlyoffice-nonce-file";
+        #   jwtSecretFile = "/run/secrets/onlyoffice-jwt-secret";
+        # };
 
         services.nginx.virtualHosts.${config.services.nextcloud.hostName} = {
           forceSSL = true;

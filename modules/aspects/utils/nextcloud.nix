e5b9a334dbf3b4c57ec84be00a57f0d0d14de54f
Aniket Bhattacharyea <aniket@abhattacharyea.dev>
2026-07-18 15:22:50 +0530

    Update

--- modules/aspects/utils/nextcloud.nix
+++ modules/aspects/utils/nextcloud.nix
@@ -73,7 +73,7 @@ _: {
             recognize = pkgs.fetchNextcloudApp {
               hash = "sha256-x3LXZKDWmzCYLTaNqSvgu4Gvrn6w2c/jifNCx1oaw1U=";
               url = "https://github.com/nextcloud/recognize/releases/download/v11.0.1/recognize-11.0.1.tar.gz";
-              licenese = "agpl";
+              license = "agpl";
             };
           };
           extraAppsEnable = true;

df7c1f6e4dec2f216c7befa8fc7f085f19805361
Aniket Bhattacharyea <aniket@abhattacharyea.dev>
2026-09-10 20:07:09 +0530

    Add nixflix

--- modules/aspects/media/nixflix.nix
+++ modules/aspects/media/nixflix.nix
@@ -74,7 +74,7 @@
         torrentClients = {
           qbittorrent = {
             enable = true;
-            password = config.sops.secrets."torrent/qbittorrent/password".path;
+            password._secret = config.sops.secrets."torrent/qbittorrent/password".path;
             serverConfig.Preferences.WebUI.Password_PBKDF2 = "@ByteArray(283lbAoUgtn5pkt16VvpgA==:+e1dH4JAJg5ZoAkeC5rnQiW+hCNGSLt6ZHnYY2Oueo5rHMQU+3l8queIiGhYaJWi1Zei1iYr67vdBL7zKYpTpw==)";
           };
         };
